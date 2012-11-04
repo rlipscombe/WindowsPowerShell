08d4414d5523749999a4cd97ae124c041f0bbbdb
Roger Lipscombe <roger@differentpla.net>
2012-11-04 19:23:09 +0000

    Fix default encoding in ISE.

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,16 @@
+﻿function Set-Encoding($file) {
+	$file.GetType().GetField("encoding", "NonPublic, Instance").SetValue($file, [Text.Encoding]::UTF8)
+}
+
+Register-ObjectEvent `
+	$psISE.CurrentPowerShellTab.Files `
+    CollectionChanged `
+	-Action {
+		$event.Sender | % {
+			Set-Encoding $_ [System.Text.Encoding]::UTF8
+		}
+	}
+
+$psISE.CurrentPowerShellTab.Files | % {
+	Set-Encoding $_ [System.Text.Encoding]::UTF8
+}
